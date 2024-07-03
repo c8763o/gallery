cfac608a2b6bf5960d8527d6943730a2a0f8c5aa
c8763o <101375171+c8763o@users.noreply.github.com>
2024-07-03 15:21:21 +0800

    16th
    刷新修正

--- js/script.js
+++ js/script.js
@@ -12,3 +12,19 @@ $('#container').imagesLoaded()
         var result = image.isLoaded ? 'loaded' : 'broken';
         console.log('image is ' + result + ' for ' + image.img.src);
     });
+
+    window.onload=function () {
+        var url = document.location.href;
+        if(url.indexOf("refreshed=")==-1){
+            var time = new Date();
+            window.location.href=url+"?refreshed="+time.getTime();
+        }
+    }
+
+
+// for (var i = 1; i > 0; i--) {
+//     console.log(i)
+//     setTimeout(function () {
+//         location.reload();
+//     }, 3000);
+// }
